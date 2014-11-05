3d45ef1c935ab142d6e7e399a997565da037550a
Anton P. Linevich <anton@linevich.com>
2014-11-05 06:10:11 -0800

    amazing fixes

--- server.py
+++ server.py
@@ -1,13 +1,38 @@
+#!/usr/bin/env python
+
+import os
+import shutil
+
 from flask import Flask
 from flask import request
 app = Flask(__name__)
 
+@app.route("/crossdomain.xml")
+def cdxml():
+    return app.send_static_file('crossdomain.xml')
+
 @app.route("/upload/<filename>",methods=["POST"])
 def savepng(filename):
-    with open(("img%0" + str(len(request.headers["Frames-Total"])) + "d") % (int(request.headers["Frame"]), ) + ".png", "a") as png:
+    totalFrames = request.headers["Frames-Total"]
+    frame = int(request.headers["Frame"])
+    streamid = "mystreamid"
+    streamdir = streamid + "/"
+
+    if (not os.path.isdir(streamdir)):
+        os.mkdir(streamdir)
+
+    framesHas = len(filter(lambda f: f.endswith('.png'), os.listdir(streamid + '/')))
+
+    outfn = (streamid + "/img%0" + str(len(totalFrames)) + "d") % (frame,) + ".png"
+    with open(outfn, "a") as png:
         png.write(request.data)
+    print("=== got frame {0} from {1}. has {2}".format(frame,totalFrames, framesHas))
+
+    if (totalFrames == framesHas):
+        print "THEEND for " + streamid
+
     return 'ok'
 
 if __name__ == "__main__":
-    print 'gagaga'
     app.run(debug=True)
+
